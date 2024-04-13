cf6bb6679fddd14b4666e7e06665450ad4be39bc
Leandro Daniel Schugurensky <47997052+Schugu@users.noreply.github.com>
2024-04-13 17:14:39 -0300

    Botón editar

--- src/Interface.jsx
+++ src/Interface.jsx
@@ -6,6 +6,9 @@ export const Interface = ({ valorInput, setValorInput }) => {
   const [servicio, setServicio] = useState('');
   const [subs, setSubs] = useState([]);
 
+  const [editMode, setEditMode] = useState(false);
+  const [editedIndex, setEditedIndex] = useState(null);
+
   const valorDisp = valorInput - valorGastado;
 
   const handleChangeSelect = (e) => {
@@ -18,26 +21,38 @@ export const Interface = ({ valorInput, setValorInput }) => {
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    if (valorServicio > 0 && valorServicio <= valorDisp) {
-      setValorGastado(valorGastado + parseFloat(valorServicio));
-      setSubs([...subs, { servicio: servicio, valor: valorServicio }]);
-      setValorServicio('');
-    } else (
-      alert('Error!')
-    )
+    const nuevoValor = parseFloat(valorServicio);
+
+    if (editMode && nuevoValor > 0 && servicio !== '' && nuevoValor <= valorInput) {
+      // Modo de edición
+      const editedSubs = subs.map((item, index) => {
+        if (index === editedIndex) {
+          return { servicio: servicio, valor: valorServicio };
+        }
+        return item;
+      });
+      setSubs(editedSubs);
+      setEditMode(false); // Desactiva el modo de edición
+      setEditedIndex(null); // Reinicia el índice editado
+      const valorActual = subs[editedIndex].valor;
+      setValorGastado(valorGastado - valorActual + nuevoValor); // Actualiza el valor gastado
+    } else {
+      // Modo de agregar nuevo elemento
+      if (valorServicio > 0 && valorServicio <= valorDisp && servicio !== '') {
+        setValorGastado(valorGastado + nuevoValor);
+        setSubs([...subs, { servicio: servicio, valor: valorServicio }]);
+      } else {
+        alert('Error: El nuevo valor supera el presupuesto');
+      }
+    }
+    setValorServicio('');
+    setServicio('');
   }
 
+
+
+
   const subscripciones = () => {
-    const handleEdit = (index, sub) => {
-      console.log('Editar suscripción en el índice:', index, sub.servicio);
-    }
-  
-    const handleDelete = (index, sub) => {
-      setValorGastado(valorGastado - sub.valor);
-      const newSubs = subs.filter((_, i) => i !== index);
-      setSubs(newSubs);
-    }
-  
     const serviciosMapeados = {
       netflix: 'Netflix',
       disneyPlus: 'Disney+',
@@ -48,23 +63,41 @@ export const Interface = ({ valorInput, setValorInput }) => {
       appleTv: 'Apple TV',
       youtubePremium: 'YouTube Premium'
     };
-  
+
+    const handleDelete = (index, sub) => {
+      const confirm = window.confirm(`Desea eliminar la suscripción ${serviciosMapeados[sub.servicio]}, con un valor de $${sub.valor}?`);
+
+      if (confirm) {
+        setValorGastado(valorGastado - sub.valor);
+        const newSubs = subs.filter((_, i) => i !== index);
+        setSubs(newSubs);
+      }
+    }
+
+    const handleEdit = (index, sub) => {
+      console.log('Editar suscripción en el índice:', index, sub.servicio);
+      setServicio(sub.servicio);
+      setValorServicio(sub.valor);
+      setEditMode(true); // Activa el modo de edición
+      setEditedIndex(index); // Guarda el índice del elemento editado
+    }
+
     return (
       <div className="subscripciones">
         <h2 className="h2Subscripciones">Subscripciones</h2>
-  
+
         {subs.map((sub, index) => (
           <div key={index} className="sub">
             <section className="subImgCont">
               <img src={`../public/subs/${sub.servicio}.svg`} alt={sub.servicio} className="subImg" />
             </section>
-  
+
             <section className="subText">
               <span>
                 Servicio: {serviciosMapeados[sub.servicio]}: <b>${sub.valor}</b>.
               </span>
             </section>
-  
+
             <section className="subButtons">
               <button onClick={() => handleEdit(index, sub)}>Editar</button>
               <button onClick={() => handleDelete(index, sub)}>Borrar</button>
