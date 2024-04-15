d561fb54f696cd893fd66072b488f0f3c98de1a1
Leandro Daniel Schugurensky <47997052+Schugu@users.noreply.github.com>
2024-04-15 17:44:29 -0300

    Mini error fixeado

--- src/components/Portada/Portada.jsx
+++ src/components/Portada/Portada.jsx
@@ -1,7 +1,8 @@
+import PropTypes from 'prop-types';
 import { FormAñadirPlata } from '../FormAñadirPlata/FormAñadirPlata.jsx';
 import './Portada.css';
 
-const Portada = ({ onSendData }) => {
+export const Portada = ({ onSendData }) => {
   return (
     <section className='portadaSection'>
       <article className='portadaArticle'>
@@ -11,5 +12,7 @@ const Portada = ({ onSendData }) => {
     </section>
   );
 };
+Portada.propTypes = {
+  onSendData: PropTypes.func.isRequired // Aquí estás validando que onSendData sea una función y que sea requerida
+};
 
-export default Portada;
